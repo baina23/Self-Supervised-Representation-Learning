f67d8a940f26b07c03d4415118750df32d257e23
Yujie Zhang <yujiezhang@smail.nju.edu.cn>
2021-04-04 04:21:53 +0800

    Fixed item() issue

--- RotNet/algorithms/ClassificationModel.py
+++ RotNet/algorithms/ClassificationModel.py
@@ -29,7 +29,7 @@ def accuracy(output, target, topk=(1,)):
     res = []
     for k in topk:
         correct_k = correct[:k].view(-1).float().sum(0)
-        res.append(correct_k.mul_(100.0 / batch_size))
+        res.append(correct_k.mul_(100.0 / batch_size).item())
     return res
 
 class ClassificationModel(Algorithm):
@@ -75,8 +75,8 @@ class ClassificationModel(Algorithm):
         #*************** COMPUTE LOSSES *************************
         record = {}
         loss_total = self.criterions['loss'](pred_var, labels_var)
-        record['prec1'] = accuracy(pred_var.data, labels, topk=(1,))[0][0]
-        record['loss'] = loss_total.data[0]
+        record['prec1'] = accuracy(pred_var.data, labels, topk=(1,))[0]
+        record['loss'] = loss_total.data.item()
         #********************************************************
 
         #****** BACKPROPAGATE AND APPLY OPTIMIZATION STEP *******
